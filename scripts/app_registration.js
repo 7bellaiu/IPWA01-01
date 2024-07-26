f698294cf2a41cfc82dc3a3942de6ddc226c4756
7bellaiu <daniela.bodner@iu-study.org>
2024-07-26 15:25:59 +0200

    Registrierungsformular und Javascript zum Ein-/Ausblenden der Abholadresse

---
+++ scripts/app_registration.js
@@ -0,0 +1,17 @@
+function togglePickupAdress(){
+//wenn Abholung ausgewählt --> Adresseingabe einblenden & enablen
+//sonst --> Adresseingabe ausblenden & disablen
+    if(document.getElementById("rb_pickup").checked){
+        document.getElementById("pickupAdressFieldset").classList.remove("d-none");
+        document.getElementById("pickupAdressFieldset").disabled = false;
+        document.getElementById("in_postcode").required = true; 
+        document.getElementById("in_city").required = true; 
+        document.getElementById("in_street").required = true; 
+    }else{
+        document.getElementById("pickupAdressFieldset").classList.add("d-none");
+        document.getElementById("pickupAdressFieldset").disabled = true;
+        document.getElementById("in_postcode").required = false; 
+        document.getElementById("in_city").required = false; 
+        document.getElementById("in_street").required = false; 
+    }
+}
